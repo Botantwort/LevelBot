b5cc6d37313e9fb78f4979f44799c0a6d6cb242f
Botantwort <videomakerzwei@gmail.com>
2021-08-17 15:16:20 +0200

    erweiterung von ping

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
 	  message.channel.send('Kleiner Moment...').then((resultMessage) => {
 		const ping = resultMessage.createdTimestamp - message.createdTimestamp
   
-		resultMessage.edit(`Bot Verzögerung: ${ping} ms, API Verzögerung: ${client.ws.ping} ms.`)
+		resultMessage.edit(`Bot Verzögerung: ${ping} ms, API Verzögerung: ${client.ws.ping} ms. Mehr Informationen falls es wen interessiert: https://stats.uptimerobot.com/4vVYDSJw8j/788825420`)
 	  })
 	},
   }
