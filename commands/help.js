2daa2f99c02856eb96f7759557a452c1a35d87b0
Botantwort <videomakerzwei@gmail.com>
2021-08-31 19:19:20 +0200

    schöneres Help

--- commands/help.js
+++ commands/help.js
@@ -3,11 +3,11 @@ module.exports = {
 	description: 'help halt',
 	execute(message, args, client) {
 		if (!message.member.hasPermission("ADMINISTRATOR")) {
-		message.channel.send("°leaderboard <nummer> für die Top <nummer>; °xp um die Xp, und wie viel noch zur nächsten xprole fehlt, anzuzeigen; °level oder °rank für den rank.");
+		message.channel.send("**°leaderboard <nummer>** um die Top <nummer> anzuzeigen (max. 10)\n**°xp** um die Xp, und wie viel noch zur nächsten xprole fehlt, anzuzeigen\n**°level** oder **°rank** für den rank\n**°pass** um den Abstand zweier Personen anzuzeigen");
 		};
 		
 		if (message.member.hasPermission("ADMINISTRATOR")) {
-		message.channel.send("°leaderboard <nummer> für die Top <nummer>; °xp um die Xp, und wie viel noch zur nächsten xprole fehlt, anzuzeigen; °level oder °rank für den rank und °edit um das Level oder die XP eines Users zu verändern");
+		message.channel.send("**°leaderboard <nummer>** um die Top <nummer> anzuzeigen (max. 10)\n**°xp** um die Xp, und wie viel noch zur nächsten xprole fehlt, anzuzeigen\n**°level** oder **°rank** für den rank\n**°pass** um den Abstand zweier Personen anzuzeigen\n**°edit** um das Level oder die XP eines Users zu verändern");
 		}
 		}
 };
